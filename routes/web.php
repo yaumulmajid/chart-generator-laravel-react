43acdb5cf7ba9a861ae72b4e81eeab946da8b129
yaumul.majid <yaumulmajid@gmail.com>
2024-10-22 21:49:28 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('react/public/index.html'));
+})->where('any', '.*');
+
+Route::get('/symlink', function () {
+    $result = Artisan::call('storage:link');
+    
+    if ($result === 0) {
+        return 'Storage linked successfully!';
+    } else {
+        return 'Failed to link storage. Please check the logs for details.';
+    }
 });
